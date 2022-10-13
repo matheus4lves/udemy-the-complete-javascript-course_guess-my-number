24b7edfc33f057d2f16e1a5075c5e32fad925c1c
Matheus Alves <matheusalves.bmd@gmail.com>
2022-10-12 23:48:21 -0300

    Create a function to start a new game

---
+++ script.js
@@ -0,0 +1,30 @@
+// DOM manipulation
+const number = document.querySelector(".number");
+const message = document.querySelector(".message");
+const score = document.querySelector(".score");
+const highscore = document.querySelector(".highscore");
+const btnAgain = document.querySelector(".again");
+const btnCheck = document.querySelector(".check");
+const guess = document.querySelector(".guess");
+
+let randomNumber;
+let scoreValue;
+let highscoreValue = 0;
+
+highscore.textContent = highscoreValue;
+
+// Functionality definition
+const generateRandomNumber = () => Math.floor(Math.random() * 20 + 1);
+
+const startNewGame = () => {
+  randomNumber = generateRandomNumber();
+  scoreValue = 20;
+  number.textContent = "?";
+  message.textContent = "Start guessing ...";
+  score.textContent = scoreValue;
+  highscore.textContent = highscoreValue;
+  document.body.style.backgroundColor = "#222";
+  guess.value = "";
+};
+
+startNewGame();
